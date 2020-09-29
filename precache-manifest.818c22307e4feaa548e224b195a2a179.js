89daa5b5e4a6872a39d76bf48f6983243a304312
Kuba <jklasecki@gmail.com>
2020-09-29 18:40:14 +0200

    Updates

---
+++ precache-manifest.818c22307e4feaa548e224b195a2a179.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9582bf71252ff3c6ba95433e9169ecaf",
+    "url": "/scopeweb-2/index.html"
+  },
+  {
+    "revision": "706b4f0c11dae67877fe",
+    "url": "/scopeweb-2/static/css/main.2e539281.chunk.css"
+  },
+  {
+    "revision": "6678b0eede34d3f4724d",
+    "url": "/scopeweb-2/static/js/2.48aa241e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scopeweb-2/static/js/2.48aa241e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706b4f0c11dae67877fe",
+    "url": "/scopeweb-2/static/js/main.8311729c.chunk.js"
+  },
+  {
+    "revision": "15d85debaa3787182747",
+    "url": "/scopeweb-2/static/js/runtime-main.0fced4e8.js"
+  },
+  {
+    "revision": "da9c56c47e031e7291c5ab1884e667fa",
+    "url": "/scopeweb-2/static/media/apple.da9c56c4.svg"
+  },
+  {
+    "revision": "1967bd6c78dc2cfd149b722f7efc5b6b",
+    "url": "/scopeweb-2/static/media/behance.1967bd6c.svg"
+  },
+  {
+    "revision": "7b89be2da1ee6a67e96bbc740b084280",
+    "url": "/scopeweb-2/static/media/bookmark.7b89be2d.svg"
+  },
+  {
+    "revision": "cd4efec970eb0090672194675bac6ee9",
+    "url": "/scopeweb-2/static/media/companyPosts.cd4efec9.png"
+  },
+  {
+    "revision": "3c985fbecb2b31bb217fd3de5b1edd6b",
+    "url": "/scopeweb-2/static/media/companyPostsDark.3c985fbe.png"
+  },
+  {
+    "revision": "795ea9774c61434ac710408ba8626894",
+    "url": "/scopeweb-2/static/media/email.795ea977.svg"
+  },
+  {
+    "revision": "b184ff583f0c6f532a63dc86ab6214ef",
+    "url": "/scopeweb-2/static/media/exploreSection.b184ff58.png"
+  },
+  {
+    "revision": "a62462df877048a7ddc779a16012e2a0",
+    "url": "/scopeweb-2/static/media/exploreSectionDark.a62462df.png"
+  },
+  {
+    "revision": "ac75d3b3a7ffadba95648660767cd9a1",
+    "url": "/scopeweb-2/static/media/face1.ac75d3b3.jpg"
+  },
+  {
+    "revision": "28119564dcea9371248a7cf690d7a09e",
+    "url": "/scopeweb-2/static/media/face2.28119564.jpg"
+  },
+  {
+    "revision": "ad154836c0ffb4d2e9d0e903f00d1599",
+    "url": "/scopeweb-2/static/media/face3.ad154836.jpg"
+  },
+  {
+    "revision": "c14da210ece1241c23521e65ebb6dad9",
+    "url": "/scopeweb-2/static/media/facebookThin.c14da210.svg"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/scopeweb-2/static/media/google.09aea0f5.svg"
+  },
+  {
+    "revision": "7a7dd387a6163243744114bdcdb4c27f",
+    "url": "/scopeweb-2/static/media/googlePlay.7a7dd387.svg"
+  },
+  {
+    "revision": "61a82ec5b93dc0e13ca17f44c554c359",
+    "url": "/scopeweb-2/static/media/hamburger.61a82ec5.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/scopeweb-2/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "b6c0769ab80a0f28fb0c654bad2b8350",
+    "url": "/scopeweb-2/static/media/instagramThin.b6c0769a.svg"
+  },
+  {
+    "revision": "5c3f7cfcf9ac810e39cd13dd85ddcdc8",
+    "url": "/scopeweb-2/static/media/landingPage.5c3f7cfc.png"
+  },
+  {
+    "revision": "bd9fe95295960dbe5110920c19207f28",
+    "url": "/scopeweb-2/static/media/logo.bd9fe952.svg"
+  },
+  {
+    "revision": "40d59f6c1c9311a4ed0995bc21cd9ac8",
+    "url": "/scopeweb-2/static/media/logoWhite.40d59f6c.svg"
+  },
+  {
+    "revision": "f1e5a05df7d29acec02433a3605a9c0a",
+    "url": "/scopeweb-2/static/media/megaphone.f1e5a05d.svg"
+  },
+  {
+    "revision": "00117923d05390016c2c63292e0fe71a",
+    "url": "/scopeweb-2/static/media/mobileApp.00117923.png"
+  },
+  {
+    "revision": "bb874b9ae0a1d954521f9a8df0295abd",
+    "url": "/scopeweb-2/static/media/mobileAppDark.bb874b9a.png"
+  },
+  {
+    "revision": "5ad5182317a1d81e1a72aa0c3ef614b3",
+    "url": "/scopeweb-2/static/media/moon.5ad51823.svg"
+  },
+  {
+    "revision": "6719431d6075f3639ff5bd84a5a34b79",
+    "url": "/scopeweb-2/static/media/pin.6719431d.svg"
+  },
+  {
+    "revision": "96dd92847efc06e17b1d964ce47a463d",
+    "url": "/scopeweb-2/static/media/quotes.96dd9284.svg"
+  },
+  {
+    "revision": "4b713cfeb6d51be8acd2325c8b8b456a",
+    "url": "/scopeweb-2/static/media/right-arrow.4b713cfe.svg"
+  },
+  {
+    "revision": "c2eef876fe99685b9bfe1c1423363fc2",
+    "url": "/scopeweb-2/static/media/rugby.c2eef876.svg"
+  },
+  {
+    "revision": "ca028d96313baa7d8d6c73fde4fdad77",
+    "url": "/scopeweb-2/static/media/send.ca028d96.svg"
+  },
+  {
+    "revision": "58c341f068259cb955f5a68d69a35322",
+    "url": "/scopeweb-2/static/media/sun.58c341f0.svg"
+  },
+  {
+    "revision": "783040f8e347608c4aba93bde8510017",
+    "url": "/scopeweb-2/static/media/tech.783040f8.svg"
+  },
+  {
+    "revision": "e6a5052318d261539243eaf732e6bf9a",
+    "url": "/scopeweb-2/static/media/twitterThin.e6a50523.svg"
+  },
+  {
+    "revision": "3db3686bd631ec05f3eb85b9b7afcdc3",
+    "url": "/scopeweb-2/static/media/windows.3db3686b.svg"
+  }
+]);
